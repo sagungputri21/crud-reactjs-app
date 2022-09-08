26374bb722335ae56a2e013e64797e525a8f6af1
Sagung Putri Nariswari <sagungputri1530@gmail.com>
2022-09-09 02:57:50 +0700

    develop code per section

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../Navbar/Sidebar'
+
+const PageLayout = ({ children }) => {
+  return (
+    <div className="flex flex-row bg-brand-black-primary font-Inter overflow-hidden">
+        <Sidebar/>
+        <div className="block w-full p-8">
+            { children }
+        </div>
+    </div>
+  )
+}
+
+export default PageLayout
